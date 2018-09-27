46d47f7da305be5304eeb7b46557c36fa8a834af
lad5791 <lad5791@g.rit.edu>
2018-09-27 15:41:33 -0400

    I started a new branch

---
+++ app/src/main/java/com/example/colby/helloworld/activity_lad5791.java
@@ -0,0 +1,13 @@
+package com.example.colby.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_lad5791 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lad5791);
+    }
+}
