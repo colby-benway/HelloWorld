a52ee8abb295cdc01b736f4a483a603456abd081
ejc2140 <ejc2140@rit.edu>
2018-10-04 15:32:01 -0400

    Tutorial 5

--- app/src/main/java/com/example/colby/helloworld/EmilyCrilley.java
+++ app/src/main/java/com/example/colby/helloworld/EmilyCrilley.java
@@ -1,13 +1,29 @@
 package com.example.colby.helloworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EmilyCrilley extends AppCompatActivity {
-
+    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emily_crilley);
+
+        button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openMainActivity();
+            }
+        });
+    }
+    public void openMainActivity(){
+        Intent intent = new Intent(packageContext: this, MainActivity.class);
+        startActivity(intent);
     }
 }
