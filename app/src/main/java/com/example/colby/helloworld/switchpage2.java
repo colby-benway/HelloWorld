3c0a7849ff3bc67c790b7670d65886f409fc12ec
colby-benway <cdb3832@rit.edu>
2018-10-04 15:26:33 -0400

    trying to see if it works this time. doubtful.

--- app/src/main/java/com/example/colby/helloworld/switchpage2.java
+++ app/src/main/java/com/example/colby/helloworld/switchpage2.java
@@ -1,34 +1,32 @@
 package com.example.colby.helloworld;
 
-import  android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    private Button button;
+public class switchpage2 extends AppCompatActivity {
+    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_switchpage2);
 
-        button = (Button) findViewById(R.id.button2);
+        button2 = (Button) findViewById(R.id.button3);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
 
                 openActivity2();
             }
 
         });
     }
-
     public void openActivity2(){
 
-        Intent intent = new Intent(this, switchpage2.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
