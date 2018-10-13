2b7934cb92e907fbcf9ac9233750bc0a315c7b24
Bradley-B <brad.boxer1@gmail.com>
2018-10-13 16:54:59 -0400

    Update MainActivity and Create Round Button
    this commit is mostly a pr tst

--- app/src/main/java/com/example/colby/helloworld/MainActivity.java
+++ app/src/main/java/com/example/colby/helloworld/MainActivity.java
@@ -15,19 +15,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = (Button) findViewById(R.id.button2);
-
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-
                 openActivity2();
             }
-
         });
     }
 
     public void openActivity2(){
-
         Intent intent = new Intent(this, switchpage2.class);
         startActivity(intent);
     }
