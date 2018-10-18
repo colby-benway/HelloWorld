ced74070f207971f0d8a12bd77bc868c36e56dff
Bradley-B <brad.boxer1@gmail.com>
2018-10-18 12:23:51 -0400

    Maps activity and updated Welcome Activity

---
+++ app/src/main/java/com/example/colby/helloworld/MapsActivity.java
@@ -0,0 +1,102 @@
+package com.example.colby.helloworld;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+
+            @Override
+            public View getInfoWindow(Marker arg0) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+                Context mContext = getApplicationContext();
+
+                LinearLayout info = new LinearLayout(mContext);
+                info.setOrientation(LinearLayout.VERTICAL);
+
+                TextView title = new TextView(mContext);
+                title.setTextColor(Color.BLACK);
+                title.setGravity(Gravity.CENTER);
+                title.setTypeface(null, Typeface.BOLD);
+                title.setText(marker.getTitle());
+
+                TextView snippet = new TextView(mContext);
+                snippet.setTextColor(Color.GRAY);
+                snippet.setText(marker.getSnippet());
+
+                info.addView(title);
+                info.addView(snippet);
+
+                return info;
+            }
+        });
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Party in Sydney")
+                .snippet("Where: The Opera House\nWhen: 6:00pm"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 10f));
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney2 = new LatLng(-34.2, 151.1);
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney2)
+                .title("Second Event")
+                .snippet("Where: Everywhere\nWhen: Always"));
+
+        LatLng sydney3 = new LatLng(-33.9, 151.05);
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney3)
+                .title("Second Event")
+                .snippet("Where: Everywhere\nWhen: Always"));
+
+    }
+}
--- app/src/main/java/com/example/colby/helloworld/WelcomeActivity.java
+++ app/src/main/java/com/example/colby/helloworld/WelcomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.colby.helloworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,30 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void launchMap(View v) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchCalendar(View v) {
+        //Intent intent = new Intent(this, MapActivity.class);
+        //startActivity(intent);
+    }
+
+    public void launchDiscover(View v) {
+        //Intent intent = new Intent(this, MapActivity.class);
+        //startActivity(intent);
+    }
+
+    public void launchCreateEvent(View v) {
+        //Intent intent = new Intent(this, CreateEventActivity.class);
+        //startActivity(intent);
+    }
+
+    public void launchYourEvents(View v) {
+        //Intent intent = new Intent(this, MapActivity.class);
+        //startActivity(intent);
     }
 }
