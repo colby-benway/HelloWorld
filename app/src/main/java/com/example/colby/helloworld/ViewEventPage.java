faa622dc12071466f9d241d86220eb8cad2aad89
Matt Galan <mtg4006@g.rit.edu>
2018-10-18 13:07:20 -0400

    Added 'view event' page

---
+++ app/src/main/java/com/example/colby/helloworld/ViewEventPage.java
@@ -0,0 +1,13 @@
+package com.example.colby.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewEventPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_event_page);
+    }
+}
